f3055a61fb649858702998b1a177153c53b3f074
Joan Leon <joanleonrs@gmail.com>
2020-03-11 00:37:51 -0500

    chore(app):adding sass

---
+++ webpack-getting-started/preprocessors/src/js/index.js
@@ -0,0 +1,16 @@
+//import '../css/index.css'
+import '../../src/sass/sass.scss'
+
+document.body.innerHTML = `
+<div class="flex-container">
+  <div class="flex-item">
+    <p class="custom-text">Text One</p>
+  </div>
+  <div class="flex-item">
+    <p class="custom-text">Text Two</p>
+  </div>
+  <div class="flex-item">
+    <p class="custom-text">Text Three</p>
+  </div>
+</div>
+`;
---
+++ webpack-getting-started/preprocessors/webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    home: path.resolve(__dirname, 'src/js/index.js'),
+  },
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCSSExtractPlugin({
+      filename: 'css/[name].css',
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Plugins',
+    }),
+  ],
+};
